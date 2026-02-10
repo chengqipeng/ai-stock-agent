d425635253a839e48661d29fdd1180b8b108fa10
chengqp <chengqp@neocrm.com>
2026-02-10 14:06:49 +0800

    前端界面操作优化

--- bak/big_prompt_encoder.py
+++
@@ -1,7 +0,0 @@
-from urllib.parse import quote
-
-def encode_like_big_prompt(text):
-    """与big_prompt.py相同的编码方式：URL编码但保留特定字符"""
-    safe = '!&()*-.0123456789=ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz'
-    return quote(text, safe=safe)
-
