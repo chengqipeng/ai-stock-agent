8ee6c4bc08c030f424106ac72e607b629afc68b1
chengqp <13524555201@139.com>
2026-03-06 08:36:42 +0800

    批量拉取数据

--- start_web.sh
+++ start_web.sh
@@ -6,5 +6,5 @@ source .venv/bin/activate
 pid=$(pgrep -f "python web_app.py")
 [ -n "$pid" ] && kill "$pid" && echo "Killed pid=$pid"
 
-nohup python web_app.py > app.log 2>&1 &
+nohup python3.11 web_app.py > app.log 2>&1 &
 echo "Started, pid=$!, log=app.log"
